a1a0d5dd4a4a57b9899181874539e9dad3ff04da
DaviRdaSilva <davi.rsilva@sptech.school>
2024-04-18 21:17:54 -0300

    feat - Adicionando os cards dinamicamente na tela através de Javascript

--- js/buscar.js
+++ js/buscar.js
@@ -1,67 +1,48 @@
+async function buscar() {
+  console.log("Passei por aqui");
 
-async function buscar(){
+  const resposta = await fetch(
+    "https://660f44af356b87a55c510d81.mockapi.io/agendas"
+  );
 
-    console.log("Passei por aqui");
-    
-    const resposta = await fetch("https://660f44af356b87a55c510d81.mockapi.io/agendas");
+  const respostaDadosAgenda = await resposta.json();
 
-    const respostaDadosAgenda = await resposta.json();
+  console.log("Respota: ", respostaDadosAgenda);
 
-    console.log("Respota: ",respostaDadosAgenda);
+  const cards = document.getElementById("cards_games");
 
-    const cards = document.getElementById("cards_games");
-
-    cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
-        var card = document.createElement("div");
-        card.className = "cardItem";
-    
-        var cardIMagen = document.createElement("div");
-        cardIMagen.className = "dataGame";
-        card.appendChild(cardIMagen);
-    
-        var cardDescricao = document.createElement("div");
-        cardDescricao.className = "descricao";
-        card.appendChild(cardDescricao);
-        var cardAssisitr = document.createElement("div");
-        cardAssisitr.className = "assistir";
-        card.appendChild(cardAssisitr);
-
-        return card
-    });
-
-    /*
-    <div class="cardItem">
-          <div class="dataGame">
-            <img src="../assets/imagens/calendar-solid.svg" alt="" /> 20/04 21:30
-          </div>
-          <img class="img-background" src="../assets/imagens/jogo-padrao.png" alt="" />
-          <div class="descricao">
-            <p><strong>Nome do Jogo</strong></p>
-            <p>
-              Descrição de como vai funcionar o GamePlay, frase legal para
-              chamar a galera :D
-            </p>
-            <p><strong>Gamers: </strong></p>
-
-            <div class="gamers">
-              <div class="gamerItem">@nick01</div>
-              <div class="gamerItem">@nick02</div>
-              <div class="gamerItem">@nick03</div>
-              <div class="gamerItem">@nick04</div>
-            </div>
-          </div>
-          <div class="assistir">
-            <a class="assistirItem">
-              <img src="../assets/imagens/youtube.svg" alt="" /> Assistir
-            </a>
-          </div>
+  cards.innerHTML = respostaDadosAgenda.map((itemAgenda) => {
+    return `
+      <div class="cardItem">
+        <div class="dataGame">
+          <img src="../assets/imagens/calendar-solid.svg" alt="" /> ${itemAgenda.dataJogo}
         </div>
-    */
+        <img class="img-background" src="${itemAgenda.urlImagem}" alt="" />
+        <div class="descricao">
+          <p><strong>${itemAgenda.nome}</strong></p>
+          <p>
+            ${itemAgenda.descricao}
+          </p>
+          <p><strong>Gamers: </strong></p>
 
-    
-
+          
+          <div class="gamers" id="gamers">
+          ${itemAgenda.gamers.map((jogador) => {
+            return `
+            <div class="gamerItem">@${jogador}</div>
+            `;
+          }).join('')}
+          </div>
+          
+        </div>
+        <div class="assistir">
+          <a class="assistirItem" src="${itemAgenda.urlAssistir}">
+            <img src="../assets/imagens/youtube.svg" alt="" /> Assistir
+          </a>
+        </div>
+      </div>
+    `;
+  }).join('');
 }
 
-console.log("Antes de buscar");
 buscar();
-console.log("Depois de buscar");
